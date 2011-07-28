dafb555215a4e63044dd1eab7c33a5db0284108d
Frederic Font <frederic.font@upf.edu>
2011-07-28 17:18:06 +0200

    Updated domain name to beta.freesound.org

--- freesoundLib.js
+++ freesoundLib.js
@@ -90,7 +90,7 @@ var FS = new function()
  */
 FS.FreesoundData = function(){};
 FS.FreesoundData.apiKey = false;
-FS.FreesoundData.baseUri = 'http://tabasco.upf.edu/api'; //'http://localhost:8000/api';//
+FS.FreesoundData.baseUri = 'http://beta.freesound.org/api';
 
 /**
  * Set Api key method, checks if handed api_key exists and saves this to FreesoundData.api_key
